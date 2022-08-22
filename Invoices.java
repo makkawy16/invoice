e014e319453785670ce50533e179eb242f3bfae7
makkawy16 <111737169+makkawy16@users.noreply.github.com>
2022-08-22 11:26:04 +0200

    Add files via upload

---
+++ Invoices.java
@@ -0,0 +1,13 @@
+package invoices;
+
+import javax.swing.JFrame;
+
+
+public class Invoices {
+    public static void main(String[] args) {
+       
+        Invoice frame = new Invoice();
+        frame.setVisible(true);
+    }
+    
+}
